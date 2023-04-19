f58c98a2c2635c13706dd341d6ab14d75bc14c49
ABertie <U2439307@rts-365.dk>
2023-04-19 16:34:09 +0200

    1.0 done

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
 const BIGPICTURE = document.querySelector(".rolloverGallery__bigPicture");
 const SMALLPICTURES = document.querySelector(".rolloverGallery__smallPictures");
 
-const ALLPITURES = ['img/pexels-david-bartus-1166209.jpg','img/pexels-francesco-ungaro-2325446.jpg','img/pexels-ian-beckley-2440021.jpg','img/pexels-jaime-reimer-2662116.jpg','img/pexels-tyler-lastovich-808465.jpg']
+const ALLPICTURES = ['img/pexels-david-bartus-1166209.jpg', 'img/pexels-francesco-ungaro-2325446.jpg', 'img/pexels-ian-beckley-2440021.jpg', 'img/pexels-jaime-reimer-2662116.jpg', 'img/pexels-tyler-lastovich-808465.jpg'];
 
 var index = 0;
 
-BIGPICTURE.src = ALLPITURES[index];
+BIGPICTURE.src = ALLPICTURES[index];
 
-ALLPITURES.forEach(function(element) {
+ALLPICTURES.forEach(function (element) {
     const IMG = document.createElement("img")
     IMG.src = element;
     IMG.classList.add("rolloverGallery__smallPictures--picture")
     SMALLPICTURES.appendChild(IMG);
 });
-const PITURE = document.querySelectorAll(".rolloverGallery__smallPictures--picture")
+const PICTURE = SMALLPICTURES.querySelectorAll(".rolloverGallery__smallPictures--picture")
 
+PICTURE.forEach(function (picture, i) {
+    picture.addEventListener("mouseover", function () {
+        index = i;
+        BIGPICTURE.src = ALLPICTURES[index];
+    });
+});  
